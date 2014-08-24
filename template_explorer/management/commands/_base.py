6e566bda6a56413c47456c7c1b7b3effa0f2023a
Micah Denbraver <macromicah@gmail.com>
2014-08-23 22:32:48 -0700

    Move 'origin' monkey-patch into the base command

--- template_explorer/management/commands/_base.py
+++ template_explorer/management/commands/_base.py
@@ -1,5 +1,20 @@
+from django.template import Template
 from django.core.management import base
 import django
+
+
+def patch_template_init():
+    """Monkey-patch for versions of Django where Template doesn't store origin.
+
+    See https://code.djangoproject.com/ticket/16096.
+    """
+    old_template_init = Template.__init__
+
+    def new_template_init(self, template_string, origin=None, name='<Unknown Template>'):
+        old_template_init(self, template_string, origin, name)
+        self.origin = origin
+
+    Template.__init__ = new_template_init
 
 
 class NoArgsCommand(base.NoArgsCommand):
@@ -8,3 +23,8 @@ class NoArgsCommand(base.NoArgsCommand):
         if django.VERSION < (1, 5):  # pragma: no cover
             msg += "\n"
         self.stdout.write(msg)
+
+    def execute(self, *args, **options):
+        if django.VERSION < (1, 7):  # pragma: no cover
+            patch_template_init()
+        super(NoArgsCommand, self).execute(*args, **options)
